2817289fea6055857de60dc221f6f98131be5c0a
Roberto Rozzi <80705726+RobRozzi@users.noreply.github.com>
2022-12-17 21:04:57 +0100

    Update 013_Plot_model_probabilities.R

--- Scripts/013_Plot_model_probabilities.R
+++ Scripts/013_Plot_model_probabilities.R
@@ -14,9 +14,9 @@ library(Cairo)
 # Load data                                               ####
 ##############################################################
 
-data <- read.csv('Results/PyRate_ext_rates/Plots_ext_dates')
+data <- read.csv('Results/PyRate_ext_rates/Plots_ext_rates/PyRate_model_probabilities.csv')
 
-data <- data %>% mutate(Analysis = fct_reorder(Analysis, Model, .fun='length' )) #reorder
+data <- data %>% mutate(Analysis = fct_relevel(Analysis, "from_Late_Pleistocene", "from_Pleistocene", "from_Miocene")) #reorder
 
 #Set theme
 theme_set(theme_light(base_size = 15))
@@ -31,8 +31,8 @@ plot <- ggplot(data, aes(x = fct_inorder(Model),y = P.extinction., group = Analy
   scale_color_manual(values=c("#ea9298", "#c25234", "#52352e"), labels = c("last 0.129 Ma", "last 2.58 Ma", "last 23.03 Ma"))+
   ylab("Model posterior probability")+
   ylim(c(0,1))+
-  annotate("text", x = c("2-rate","3-rate","10-rate"), y = c(0.98,0.6,0.32), 
-          label = c("0.92", "0.54", "0.26") , color=c("#ea9298", "#c25234", "#52352e"), 
+  annotate("text", x = c("3-rate","3-rate","8-rate"), y = c(0.96,0.64,0.28), 
+          label = c("0.91", "0.59", "0.22") , color=c("#ea9298", "#c25234", "#52352e"), 
           family = "Arial", size = 4, fontface="bold")+
   theme(axis.title = element_text(family = "Arial", size = 18, colour = "grey40"),
         axis.text.x = element_text(angle = 45, family = "Arial", size = 14),
@@ -45,6 +45,6 @@ plot <- ggplot(data, aes(x = fct_inorder(Model),y = P.extinction., group = Analy
 plot
 
 #Save plot as pdf
-ggsave(plot, filename = "Plot_model_probabilities.pdf", path = "Results/PyRate_ext_rates/Plots_ext_dates", width = 10, height = 5, device = cairo_pdf) 
+ggsave(plot, filename = "Plot_model_probabilities.pdf", path = "Results/PyRate_ext_rates/Plots_ext_rates", width = 10, height = 5, device = cairo_pdf) 
 
 #End of script
